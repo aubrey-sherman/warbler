662ce7f928790cb7d571a66e8105d72394efe533
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-09 11:46:43 -0700

    updated edituserprofileform

--- forms.py
+++ forms.py
@@ -50,9 +50,14 @@ class LoginForm(FlaskForm):
 class EditUserProfileForm(FlaskForm):
     """Edit User Profile Form."""
 
-    location = StringField(
-        'Location',
-        validators=[Length(max=200)]
+    username = StringField(
+        'Username',
+        validators=[InputRequired(), Length(max=30)],
+    )
+
+    email = StringField(
+        'E-mail',
+        validators=[InputRequired(), Email(), Length(max=50)],
     )
 
     bio = StringField(
@@ -60,9 +65,19 @@ class EditUserProfileForm(FlaskForm):
         validators=[Length(max=1500)]
     )
 
-    cover_photo = StringField(
+    image_url = StringField(
         '(Optional) Image URL',
         validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    header_image_url = StringField(
+        '(Optional) Image URL',
+        validators=[Optional(), URL(), Length(max=255)]
+    )
+
+    password = PasswordField(
+        'Password',
+        validators=[InputRequired(), Length(min=6, max=50)],
     )
 
 
